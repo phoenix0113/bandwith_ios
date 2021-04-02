eac9e82cecd7f62dfcc61977f6d130df0bb19a97
PyrozhenkoSerhii <serhii.pyrozhenko@nure.ua>
2021-04-02 09:04:36 +0300

    small navigation fix

--- src/navigation/welcome/index.tsx
+++ src/navigation/welcome/index.tsx
@@ -19,7 +19,11 @@ export const WelcomeNavigation = observer(() => {
   const {token} = useContext(UserServiceContext);
 
   return (
-    <WelcomeStack.Navigator headerMode="none" initialRouteName={token ? "Main" : "Welcome"}>
+    <WelcomeStack.Navigator
+      headerMode="none"
+      initialRouteName={token ? "Main" : "Welcome"}
+      screenOptions={{gestureEnabled: false}}
+    >
       <WelcomeStack.Screen name="Welcome" component={WelcomeScreen}/>
       <WelcomeStack.Screen name="Login" component={LoginScreen}/>
       <WelcomeStack.Screen name="Registration" component={RegistrationScreen}/>
